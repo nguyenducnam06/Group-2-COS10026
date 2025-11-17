7ccef41ed65e6a88752b3c15fd172c245caf7e45
nguyenducnam06 <105544406@student.swin.edu.au>
2025-11-17 19:49:35 +0700

    Update enhancements.php

--- project1/enhancements.php
+++ project1/enhancements.php
@@ -33,12 +33,14 @@ include('metadata.inc');
 
          <section class="content_background">
             <div>
-                <h2>Enhancement 2: Secure Login with Hashed Passwords</h2>
+                <h2>Enhancement 2: Secure Sign Up/Login with Hashed Passwords</h2>
                 <p>
                 The user experience and security of the website are strengthened by this improvement. 
-                Only authorized users can now access personalized content thanks to the login system's strong authentication. 
+                The company's admins can register for their accounts based on their role, their name, and their own password.
+                Only authorized users can access personalized content thanks to the login system's authentication. 
                 Successful logins also result in customized sessions that give users access to features and access that are specific to them. 
-                Consequently, this enhancement offers a smooth, user-focused experience along with improved security.
+                Block users from accessing the web when they enter too many times.
+                This enhancement offers a smooth, user-focused experience along with improved security.
                 </p>
             </div>
         </section>
@@ -52,6 +54,17 @@ include('metadata.inc');
                 Additionally, users can effectively search and filter positions based on their preferences, including employment type, location, or job title. 
                 Thus, this enhancement offers a technically complex solution that improves user engagement and expedites the application process by fusing cutting-edge database-driven features with an easy-to-use user interface.
                 </p>
+            </div>
+        </section>
+
+        <section class="content_background">
+            <div>
+                <h2>Enhancement 4: Submit form for jobs, subscribe to newsletter updates interactively</h2>
+                <p>
+                Our platform uses PHP to handle the server-side processing of every form submission, guiding each piece of user input through a clear and reliable workflow.
+                MySQL acts as the structured foundation behind the scenes, offering a stable place for all collected data to be stored and organized.
+                When users submit job applications or sign up for newsletter updates, their information is carefully validated, processed, and then written into the database through secure PHP, MySQL interactions.
+                This approach allows the system to preserve every entry with intention, making the data easy to retrieve, manage, and integrate into the platform's evolving features.</p>
             </div>
         </section>
     </main>
